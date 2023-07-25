1b82b545d17469da3f4d7f0b8ef8b0c9012355d0
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 01:48:36 +0530

    basic upload page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Main from './components/Main';
-import MainBody from './components/MainBody';
-import SearchResults from './components/SearchResults';
-import WatchBody from './components/WatchBody';
-import store from './utils/store';
+import React from 'react';
+import FolderUpload from './FolderUpload';
 
 function App() {
-
-  const appRoutes= createBrowserRouter([{
-    path:'/',
-    element: <Main/>,
-    children:[{
-      path:'/',
-      element:<MainBody/>
-    },
-    {
-      path:'watch',
-      element:<WatchBody/>
-    },
-    {
-      path:'results',
-      element: <SearchResults/>
-    }]
-  }])
-
   return (
-      <Provider store={store}>
-        <RouterProvider router={appRoutes}/>
-      </Provider>
+    <div className="App">
+      <FolderUpload />
+    </div>
   );
 }
 
