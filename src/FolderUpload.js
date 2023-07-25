6f123e6e66d54904928acd1d47b3585d1c9b7390
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 03:57:44 +0530

    added upload button

--- src/FolderUpload.js
+++ src/FolderUpload.js
@@ -1,29 +1,19 @@
-import React, { useState } from 'react';
-import Dropzone from 'react-dropzone';
+import React from 'react';
 
 const FolderUpload = () => {
-  const [selectedFolder, setSelectedFolder] = useState(null);
-
-  const handleFileDrop = (acceptedFiles) => {
-    // Process the uploaded files here (e.g., send them to the server)
-    console.log(acceptedFiles);
+  const handleFileUpload = (event) => {
+    const files = event.target.files;
+    // Process the selected files here (e.g., send them to the server)
+    console.log(files);
   };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Folder Upload</h2>
-      <Dropzone onDrop={handleFileDrop} multiple={true} noClick noKeyboard>
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()} style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
-            <input {...getInputProps()} directory="" webkitdirectory="" mozdirectory="" />
-            {selectedFolder ? (
-              <p>Selected Folder: {selectedFolder}</p>
-            ) : (
-              <p>Drag 'n' drop a folder here, or click to select a folder</p>
-            )}
-          </div>
-        )}
-      </Dropzone>
+      <div style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
+        <input type="file" webkitdirectory="" directory="" multiple onChange={handleFileUpload} />
+        <p>Drag 'n' drop a folder here, or click to select a folder</p>
+      </div>
     </div>
   );
 };
