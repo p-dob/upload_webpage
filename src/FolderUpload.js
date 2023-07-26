6874bdb9a4bdcb59e9611861348d694a848bd30d
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 17:54:43 +0530

    added button

--- src/FolderUpload.js
+++ src/FolderUpload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
 
@@ -7,7 +7,7 @@ const FolderUpload = () => {
     const formData = new FormData();
     formData.append('file', acceptedFiles[0]);
 
-    axios.post('http://localhost:5001/upload', formData)
+    axios.post('http://localhost:8000/upload', formData)
       .then((response) => {
         console.log(response.data);
         // Handle success if needed
@@ -22,10 +22,11 @@ const FolderUpload = () => {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Folder Upload</h2>
       <Dropzone onDrop={handleFileDrop} multiple={false} noClick noKeyboard>
-        {({ getRootProps, getInputProps }) => (
+        {({ getRootProps, getInputProps, open }) => (
           <div {...getRootProps()} style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
             <input {...getInputProps()} />
             <p>Drag 'n' drop a file here, or click to select a file</p>
+            <button onClick={open} style={{ marginTop: '10px' }}>Select File</button>
           </div>
         )}
       </Dropzone>
