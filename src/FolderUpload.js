d1c4d4a220dea535948ddce53f99cdb00d09095f
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 18:49:00 +0530

    drag n drop and button both

--- src/FolderUpload.js
+++ src/FolderUpload.js
@@ -7,6 +7,9 @@ const FolderUpload = () => {
     const formData = new FormData();
     formData.append('file', files[0]);
 
+    // Append the folder name as a separate field (optional).
+    formData.append('folderName', files[0].webkitRelativePath);
+
     axios.post('http://localhost:5001/upload', formData)
       .then((response) => {
         console.log(response.data);
@@ -18,12 +21,37 @@ const FolderUpload = () => {
       });
   };
 
+  const handleFileSelect = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      // Append the folder name as a separate field (optional).
+      formData.append('folderName', selectedFile.webkitRelativePath);
+
+      axios.post('http://localhost:5001/upload', formData)
+        .then((response) => {
+          console.log(response.data);
+          // Handle success if needed
+        })
+        .catch((error) => {
+          console.error(error);
+          // Handle error if needed
+        });
+    }
+  };
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Folder Upload</h2>
       <FileDrop onDrop={handleFileDrop}>
         <div style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
-          <p>Drag 'n' drop a file here, or click to select a file</p>
+          <p>Drag 'n' drop a file or folder here</p>
+          <button onClick={() => document.getElementById('fileInput').click()} style={{ marginTop: '10px' }}>
+            Select File or Folder
+          </button>
+          <input id="fileInput" type="file" style={{ display: 'none' }} onChange={handleFileSelect} webkitdirectory="true" />
         </div>
       </FileDrop>
     </div>
