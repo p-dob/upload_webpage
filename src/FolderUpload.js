35f3655e1d7496c560797b984973fab97181b54d
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 16:55:29 +0530

    implementing frontend backend comm

--- src/FolderUpload.js
+++ src/FolderUpload.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
 const FolderUpload = () => {
-  const handleFileUpload = (event) => {
-    const files = event.target.files;
-    // Process the selected files here (e.g., send them to the server)
-    console.log(files);
+  const handleFileDrop = (acceptedFiles) => {
+    const formData = new FormData();
+    formData.append('file', acceptedFiles[0]);
+
+    axios.post('http://localhost:5001/upload', formData)
+      .then((response) => {
+        console.log(response.data);
+        // Handle success if needed
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error if needed
+      });
   };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Folder Upload</h2>
-      <div style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
-        <input type="file" webkitdirectory="" directory="" multiple onChange={handleFileUpload} />
-        <p>Drag 'n' drop a folder here, or click to select a folder</p>
-      </div>
+      <Dropzone onDrop={handleFileDrop} multiple={false} noClick noKeyboard>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop a file here, or click to select a file</p>
+          </div>
+        )}
+      </Dropzone>
     </div>
   );
 };
