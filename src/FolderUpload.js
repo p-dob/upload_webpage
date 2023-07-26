532fe447c5f4515d939ba55bc79fa89d7e3775e4
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 18:05:19 +0530

    minor fix

--- src/FolderUpload.js
+++ src/FolderUpload.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
+import { FileDrop } from 'react-file-drop';
 import axios from 'axios';
 
 const FolderUpload = () => {
-  const handleFileDrop = (acceptedFiles) => {
+  const handleFileDrop = (files) => {
     const formData = new FormData();
-    formData.append('file', acceptedFiles[0]);
+    formData.append('file', files[0]);
 
     axios.post('http://localhost:5001/upload', formData)
       .then((response) => {
@@ -18,37 +18,14 @@ const FolderUpload = () => {
       });
   };
 
-  const handleFileSelect = (event) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
-
-      axios.post('http://localhost:5001/upload', formData)
-        .then((response) => {
-          console.log(response.data);
-          // Handle success if needed
-        })
-        .catch((error) => {
-          console.error(error);
-          // Handle error if needed
-        });
-    }
-  };
-
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Folder Upload</h2>
-      <Dropzone onDrop={handleFileDrop} multiple={false} noClick noKeyboard>
-        {({ getRootProps, getInputProps, open }) => (
-          <div {...getRootProps()} style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop a file here, or click to select a file</p>
-            <button onClick={open} style={{ marginTop: '10px' }}>Select File</button>
-          </div>
-        )}
-      </Dropzone>
-      <input type="file" style={{ display: 'none' }} onChange={handleFileSelect} />
+      <FileDrop onDrop={handleFileDrop}>
+        <div style={{ border: '2px dashed #cccccc', padding: '20px', cursor: 'pointer' }}>
+          <p>Drag 'n' drop a file here, or click to select a file</p>
+        </div>
+      </FileDrop>
     </div>
   );
 };
