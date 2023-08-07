61e9b568eb06da27b6fc13d5d5eeeb5d7ed73f36
p-dob <88445292+p-dob@users.noreply.github.com>
2023-08-08 00:48:50 +0530

    axios drag and drop upload implementation

--- src/FolderFileUpload.js
+++ src/FolderFileUpload.js
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FileUploaderForm from './FileUploaderForm';
+import axios from 'axios'; // Import axios
 
 const FolderFileUploader = () => {
   const [files, setFiles] = useState([]);
   const [progress, setProgress] = useState(0);
   const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [topLevelFolders, setTopLevelFolders] = useState([]);
 
   const handleFileChange = (e) => {
     setFiles([...e.target.files]);
   };
 
   const handleDrop = (e) => {
-    // e.preventDefault();
-    // const items = e.dataTransfer.items;
     const items = e;
-  
+
     let folderContents = [];
-  
+    let topLevelFolders = new Set(); // Using Set to store unique top-level folder names
+
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
       if (item) {
-        folderContents.push(item.path)
+        if (item.path) {
+          // folderContents.push(item.path);
+          folderContents.push(item);
+          const fullPathParts = item.path.split("/");
+          if (fullPathParts[0] === "") {
+            fullPathParts.shift(); // Remove the first element if it is empty (due to leading '/')
+          }
+          topLevelFolders.add(fullPathParts[0]);
+        }
       }
     }
     setFiles(folderContents);
+    setTopLevelFolders(Array.from(topLevelFolders)); // Update the state with top-level folder names
   };
-
+  
   const traverseFileTree = (item, folderContents) => {
     if (item.isFile) {
       item.file((file) => {
@@ -42,46 +52,36 @@ const FolderFileUploader = () => {
       folderContents.push({ type: "folder", name: item.name, children: [] });
     }
   };
-  
-  const handleUpload = (e) => {
+
+  const handleUpload = async (e) => {
     e.preventDefault();
-    // Simulate file upload progress
-    setProgress(0);
-    const totalFiles = files.length;
-    let uploadedFiles = 0;
-  
-    const interval = setInterval(() => {
-      uploadedFiles++;
-      const currentProgress = Math.floor((uploadedFiles / totalFiles) * 100); // Convert to integer
-      setProgress(currentProgress);
-  
-      if (currentProgress === 100) {
-        clearInterval(interval);
-        setUploadedFiles([...files]); // Mark all files as uploaded
 
-        // Print every file and folder name on the console
-        files.forEach((file) => {
-          console.log(file)
-          if (file.type === "file") {
-            console.log(file.file.name);
-          } else if (file.type === "folder") {
-            console.log(file.name + "/");
-            printFilesInFolder(file.children, file.name + "/");
-          }
-        });
-      }
-    }, 200);
-  };  
-
-  const printFilesInFolder = (folder, path = '') => {
-    folder.forEach((file) => {
-      if (file.type === "file") {
-        console.log(path + file.name);
-      } else if (file.type === "folder") {
-        console.log(path + file.name + "/");
-        printFilesInFolder(file.children, path + file.name + "/");
-      }
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append('file', file);
+      formData.append('folderName', file.path);
     });
+
+
+    try {
+      const response = await axios.post('http://localhost:5001/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: (progressEvent) => {
+          const currentProgress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+          setProgress(currentProgress);
+        }
+      });
+
+      console.log(response.data); // Log the response from the backend
+
+      // Clear the uploaded files and progress
+      setUploadedFiles([...files]);
+      setProgress(0);
+    } catch (error) {
+      console.error('Error uploading files:', error);
+    }
   };
 
   return (
