2832850e12f7c7b3b530b694b810ac62cd45cff2
p-dob <88445292+p-dob@users.noreply.github.com>
2023-08-14 20:23:51 +0530

    implemented progress bar for each file and folders

--- src/FileUploaderForm.js
+++ src/FileUploaderForm.js
@@ -3,34 +3,34 @@ import { useDropzone } from 'react-dropzone';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faCircle } from '@fortawesome/free-solid-svg-icons';
 
-const FileUploaderForm = ({ files, progress, uploadedFiles, uploadInProgress, handleFileChange, handleDrop, handleUpload, topLevelFolders }) => {  
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop: handleDrop,
-    noClick: true, // To prevent the default file selection behavior when clicking inside the drop zone
-    noKeyboard: true, // To prevent the default file selection behavior when pressing keyboard keys
-  });
-  
-  const MyDropZone = () => {
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-      onDrop: handleDrop,
-      noClick: true, // Prevent file dialog from opening on click
+const FileUploaderForm = ({ files, progress, uploadedFiles, uploadInProgress, handleFileChange, handleDrop, handleUpload, topLevelFolders, folderProgress }) => {
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop: handleDrop,
+        noClick: true, // To prevent the default file selection behavior when clicking inside the drop zone
+        noKeyboard: true, // To prevent the default file selection behavior when pressing keyboard keys
     });
-  
-    return (
-        <div
-          className={`upload-drop-zone ${isDragActive ? 'active' : ''}`}
-          id="drop-zone"
-          {...getRootProps()}
-        >
-          <input {...getInputProps()} />
-          <h4>{isDragActive ? 'Drop files here' : 'Drag and drop files here, or click to select'}</h4>
-          {files.length === 0
-            ? 'No files selected'
-            : `${files.length} file(s) selected`}
-        </div>
-      );
+
+    const MyDropZone = () => {
+        const { getRootProps, getInputProps, isDragActive } = useDropzone({
+            onDrop: handleDrop,
+            noClick: true, // Prevent file dialog from opening on click
+        });
+
+        return (
+            <div
+                className={`upload-drop-zone ${isDragActive ? 'active' : ''}`}
+                id="drop-zone"
+                {...getRootProps()}
+            >
+                <input {...getInputProps()} />
+                <h4>{isDragActive ? 'Drop files here' : 'Drag and drop files here, or click to select'}</h4>
+                {files.length === 0
+                    ? 'No files selected'
+                    : `${files.length} file(s) selected`}
+            </div>
+        );
     };
-  
+
 
     return (
         <div className="container">
@@ -75,32 +75,37 @@ const FileUploaderForm = ({ files, progress, uploadedFiles, uploadInProgress, ha
                             aria-valuemin="0"
                             aria-valuemax="100"
                             style={{ width: `${progress}%` }}
-                            >
+                        >
                             {uploadInProgress ? `${progress}%` : `${progress}% Complete`}
                         </div>
                     </div>
 
                     {/* Upload Finished */}
                     <div className="js-upload-finished">
-                        <h3>Processed files</h3>
+                        <h3>Processed files and folders</h3>
                         <div className="list-group">
-                            {topLevelFolders.map((file, index) => (
-                                <a
-                                    key={index}
-                                    href="#"
-                                    className={`list-group-item ${uploadedFiles.includes(file) ? 'list-group-item-success' : ''}`}
-                                >
-                                    {uploadedFiles.includes(file) && (
-                                        <span className="badge alert-success pull-right circle-badge">
+                            {topLevelFolders.map((folderName, index) => (
+                                <div key={index} className={`list-group-item ${uploadedFiles.includes(folderName) ? 'list-group-item-success' : ''}`}>
+                                    <span className="folder-name">
+                                        {folderName}
+                                    </span>
+                                    <span className="badge alert-success pull-right circle-badge">
                                             <FontAwesomeIcon icon={faCircle} className="circle-background" />
                                             <FontAwesomeIcon icon={faCheck} className="tick-icon" />
-                                        </span>
-                                    )}
-                                    {/* Display folder name */}
-                                    <span className="file-name">
-                                        {file}
                                     </span>
-                                </a>
+                                    <div className="progress">
+                                        <div
+                                            className="progress-bar"
+                                            role="progressbar"
+                                            aria-valuenow={folderProgress[folderName]?.uploadPercent || 0}
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                            style={{ width: `${folderProgress[folderName]?.uploadPercent || 0}%` }}
+                                        >
+                                            {folderProgress[folderName]?.uploadPercent?.toFixed(2) || 0}%
+                                        </div>
+                                    </div>
+                                </div>
                             ))}
                         </div>
                     </div>
--- src/FolderFileUpload.js
+++ src/FolderFileUpload.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FileUploaderForm from './FileUploaderForm';
-import axios from 'axios'; // Import axios
+import axios from 'axios';
 import vars from './vars.json'
 
 const FolderFileUploader = () => {
@@ -10,6 +10,8 @@ const FolderFileUploader = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [topLevelFolders, setTopLevelFolders] = useState([]);
   const [uploadInProgress, setUploadInProgress] = useState(false);
+  const [fileProgress, setFileProgress] = useState({});
+  const [folderProgress, setFolderProgress] = useState({});
 
   const handleFileChange = (e) => {
     setFiles([...e.target.files]);
@@ -18,7 +20,42 @@ const FolderFileUploader = () => {
   useEffect(() => {
     setProgress(0); // Reset progress to 0 when new files are selected
   }, [files]);
-  
+
+  // useEffect to update folderProgress when fileProgress changes
+  useEffect(() => {
+    const updatedFolderProgress = {};
+
+    Object.entries(fileProgress).forEach(([filename, progressArray]) => {
+      if (filename.startsWith('/')) {
+        const folderName = filename.split('/')[1]; // Extract the folder name
+        const innerProgress = progressArray[progressArray.length - 1]; // Get the latest progress and total size
+
+        if (!updatedFolderProgress[folderName]) {
+          updatedFolderProgress[folderName] = {
+            uploadedSize: 0,
+            totalSize: 0
+          };
+        }
+
+        updatedFolderProgress[folderName][filename] = {
+          uploadedSize: innerProgress.uploadedSize,
+          totalSize: innerProgress.totalSize
+        };
+
+        // Calculate total progress and total size for the folder
+        updatedFolderProgress[folderName].uploadedSize += innerProgress.uploadedSize;
+        updatedFolderProgress[folderName].totalSize += innerProgress.totalSize;
+        updatedFolderProgress[folderName].uploadPercent = (updatedFolderProgress[folderName].uploadedSize / updatedFolderProgress[folderName].totalSize) * 100;
+      } else {
+        updatedFolderProgress[filename] = progressArray[progressArray.length - 1];
+        updatedFolderProgress[filename].uploadPercent = (updatedFolderProgress[filename].uploadedSize / updatedFolderProgress[filename].totalSize) * 100;
+      }
+    });
+
+    // Update the folderProgress state variable
+    setFolderProgress(updatedFolderProgress);
+  }, [fileProgress]);
+
   const handleDrop = (e) => {
     const items = e;
     let folderContents = [];
@@ -40,44 +77,44 @@ const FolderFileUploader = () => {
     setFiles(folderContents);
     setTopLevelFolders(Array.from(toplevelFolders)); // Update the state with top-level folder names
   };
-  
-  const traverseFileTree = (item, folderContents) => {
-    if (item.isFile) {
-      item.file((file) => {
-        folderContents.push({ type: "file", file });
-      });
-    } else if (item.isDirectory) {
-      const reader = item.createReader();
-      reader.readEntries((entries) => {
-        entries.forEach((entry) => {
-          traverseFileTree(entry, folderContents);
-        });
-      });
-      folderContents.push({ type: "folder", name: item.name, children: [] });
-    }
-  };
 
   const handleUpload = async (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    files.forEach((file) => {
-      formData.append('file', file);
-      formData.append('folderName', file.path);
-    });
-
     try {
       setUploadInProgress(true); // Set upload status to true
-      const response = await axios.post(vars['upload_endpoint'], formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        },
-        onUploadProgress: (progressEvent) => {
-          const currentProgress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-          setProgress(currentProgress);
-        }
+
+      const totalBytes = files.reduce((total, file) => total + file.size, 0); // Calculate total bytes to be uploaded
+
+      // Create an array to store promises for each upload
+      const uploadPromises = files.map((file) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('folderName', file.path);
+
+        return axios.post(vars['upload_endpoint'], formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          onUploadProgress: (progressEvent) => {
+            setFileProgress(prevProgress => {
+              let newProgress = { ...prevProgress };
+              newProgress[file.path] = [
+                ...(newProgress[file.path] || []),
+                {
+                  uploadedSize: progressEvent.loaded,
+                  totalSize: progressEvent.total
+                }
+              ];
+              return newProgress;
+            });
+          }
+        });
       });
-      console.log(response.data); // Log the response from the backend
+
+      // Use Promise.all to execute all upload promises concurrently
+      await Promise.all(uploadPromises);
+
       // Clear the uploaded files and progress
       setUploadedFiles([...files]);
       setUploadInProgress(false); // Reset upload status after successful upload
@@ -97,6 +134,7 @@ const FolderFileUploader = () => {
       handleDrop={handleDrop}
       handleUpload={handleUpload}
       topLevelFolders={topLevelFolders}
+      folderProgress={folderProgress}
     />
   );
 };
