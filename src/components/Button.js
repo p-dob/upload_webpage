870bac9bff0456d3f69272f0406a65147d3d057d
p-dob <88445292+p-dob@users.noreply.github.com>
2023-09-03 01:54:16 +0530

    removed unwanted default files

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({name}) => {
-  return (
-    <button className='m-2 h-11 px-3 py-1 bg-gray-200 rounded-2xl'>{name}</button>
-  )
-}
-
-export default Button
